977951bcac670e46777341bfa2c38547421d8c0a
enespinar <enespinar2@icloud.com>
2024-03-22 17:24:27 +0300

    configured tailwind

--- ganmimarlik/ganmimarlik/settings.py
+++ ganmimarlik/ganmimarlik/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "compressor",
     "main",
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = "ganmimarlik.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,3 +124,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Compressor
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
