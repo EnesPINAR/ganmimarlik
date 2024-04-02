fc233a48bd8d8f812800ead64249fbdf3d6b41ec
enespinar <enespinar2@icloud.com>
2024-04-02 18:52:07 +0300

    homepage improvements & logo added

--- ganmimarlik/tailwind.config.js
+++ ganmimarlik/tailwind.config.js
@@ -1,16 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-  content: [
-      './templates/**/*.html',
-      './node_modules/flowbite/**/*.js'
-  ],
-  theme: {
-    extend: {
+    content: [
+        './templates/**/*.html',
+        './node_modules/flowbite/**/*.js'
+    ],
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {
+            textShadow: {
+                sm: '0 1px 2px var(--tw-shadow-color)',
+                DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+                lg: '0 8px 16px var(--tw-shadow-color)',
+            },
+        },
     },
-  },
-  plugins: [
-      require('flowbite/plugin')
-  ],
+    plugins: [
+        require('flowbite/plugin'),
+        plugin(function ({matchUtilities, theme}) {
+            matchUtilities(
+                {
+                    'text-shadow': (value) => ({
+                        textShadow: value,
+                    }),
+                },
+                {values: theme('textShadow')}
+            )
+        }),
+        require('@tailwindcss/typography'),
+    ],
 }
 
 
