88e8b1e4f650ba9c5663c54a769718d0784684b1
EnesPINAR <enespinar2@icloud.com>
2024-04-23 00:50:10 +0300

    background added & homepage configured

--- ganmimarlik/main/models.py
+++ ganmimarlik/main/models.py
@@ -3,3 +3,35 @@ from django.db import models
 # Create your models here.
 class BannerBg(models.Model):
     image = models.ImageField(upload_to='banner/%Y/%m/%d', editable=True)
+    def __str__(self): 
+        return "Ana sayfa arkaplanı"
+
+class AnaSayfaBaslik1(models.Model):
+    content = models.CharField(max_length=20, editable=True)
+    def __str__(self):
+        return 'Ana sayfa ilk başlık'
+
+class AnaSayfaYazi1(models.Model):
+    content = models.CharField(max_length=500, editable=True)
+    def __str__(self):
+        return 'Ana sayfa ilk yazı'
+    
+class AnaSayfaGorsel1(models.Model):
+    img1 = models.ImageField(upload_to='homepage/%Y/%m/%d', editable=True)
+    def __str__(self):
+        return 'Ana sayfa ilk görsel'
+    
+class AnaSayfaBaslik2(models.Model):
+    content = models.CharField(max_length=20, editable=True)
+    def __str__(self):
+        return 'Ana sayfa ikinci başlık'
+    
+class AnaSayfaYazi2(models.Model):
+    content = models.CharField(max_length=500, editable=True)
+    def __str__(self):
+        return 'Ana sayfa ikinci yazı'
+    
+class AnaSayfaGorsel2(models.Model):
+    img2 = models.ImageField(upload_to='homepage/%Y/%m/%d', editable=True)
+    def __str__(self):
+        return 'Ana sayfa ikinci görsel'
--- ganmimarlik/main/views.py
+++ ganmimarlik/main/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from .models import BannerBg
+from . import models
 
 # Create your views here.
 def index(request):
-    bannerBg = BannerBg.objects.all()
-    return render(request, 'index.html', {'bannerBg': bannerBg})
+    bannerBg = models.BannerBg.objects.all()
+    baslik1 = models.AnaSayfaBaslik1.objects.first()
+    content1 = models.AnaSayfaYazi1.objects.first()
+    img1 = models.AnaSayfaGorsel1.objects.first()
+    baslik2 = models.AnaSayfaBaslik2.objects.first()
+    content2 = models.AnaSayfaYazi2.objects.first()
+    img2 = models.AnaSayfaGorsel2.objects.first()
+    
+    context = {
+        'bannerBg': bannerBg,
+        'baslik1': baslik1,
+        'content1': content1,
+        'img1': img1,
+        'baslik2': baslik2,
+        'content2': content2,
+        'img2': img2,
+    }
+    return render(request, 'index.html', context)
