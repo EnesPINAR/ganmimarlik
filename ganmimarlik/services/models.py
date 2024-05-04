794bb622956f9bd1a357de89367581fe60945c60
enespinar <enespinar2@icloud.com>
2024-05-05 00:17:47 +0300

    changed model name MimariServis to MimarlikServisi

--- ganmimarlik/services/models.py
+++ ganmimarlik/services/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class MimariServis(models.Model):
+class MimarlikServisi(models.Model):
     img = models.ImageField(upload_to='service/%Y/%m/%d', editable=True, verbose_name='Görsel')
     name = models.CharField(max_length=30, editable=True, verbose_name='İsim')
     content = models.TextField(max_length=250, editable=True, verbose_name='İçerik (En fazla 250 karakter)')
