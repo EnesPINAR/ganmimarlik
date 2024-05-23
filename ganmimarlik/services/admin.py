b8ba3f64eed85e441bc7ee37c946306e4d8b8c2b
enespinar <enespinar2@icloud.com>
2024-05-23 23:31:33 +0300

    services page updated

--- ganmimarlik/services/admin.py
+++ ganmimarlik/services/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.MimarlikServisi)
+admin.site.register(models.MimarlikServisiGorselleri)
 admin.site.register(models.IcMimarlikServisi)
--- ganmimarlik/services/models.py
+++ ganmimarlik/services/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class MimarlikServisi(models.Model):
-    img = models.ImageField(upload_to='service/%Y/%m/%d', editable=True, verbose_name='Görsel')
     name = models.CharField(max_length=30, editable=True, verbose_name='İsim')
     content = models.TextField(max_length=250, editable=True, verbose_name='İçerik (En fazla 250 karakter)')
 
@@ -16,3 +15,10 @@ class IcMimarlikServisi(models.Model):
 
     def __str__(self):
         return self.name + ' Servisi'
+
+class MimarlikServisiGorselleri(models.Model):
+    mimarlikservisi = models.ForeignKey(MimarlikServisi, on_delete=models.CASCADE, related_name='images')
+    img = models.ImageField(upload_to='service/%Y/%m/%d', editable=True, verbose_name='Görsel')
+
+    class Meta:
+        verbose_name_plural = 'Mimarlık Servisi Görselleri'
